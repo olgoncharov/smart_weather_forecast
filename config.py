25cf63d55786b19c3435e6e9ae40f2db51c72bf5
Oleg Goncharov <oleg.gnch@gmail.com>
2020-04-15 00:50:47 +0300

    fixed path to database

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = \
-        'sqlite:////' + os.path.join(BASE_DIR, 'geo_objects.sqlite')
+        'sqlite:///' + os.path.join(BASE_DIR, 'geo_objects.sqlite')
 
     CACHE_TYPE = 'simple'
     CACHE_DEFAULT_TIMEOUT = 300
